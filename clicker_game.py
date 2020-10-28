135828bce93fc1798f57564f731027c5bf210bd4
CoderCheese <68267100+TheRealCoderCheese@users.noreply.github.com>
2020-10-28 13:37:12 +1300

    Update clicker_game.py

--- clicker_game.py
+++ clicker_game.py
@@ -7,11 +7,11 @@ score.set(0)
 #Functions
 def upgrade():
 	global score  
-	if score.get() > 100:
+	if score.get() > 99:
 		score.set(score.get() +3 )
 def upgrade1():
 	global score 
-	if score.get() > 50:
+	if score.get() > 49:
 		score.set(score.get() + 2)
 
 def update_score():
@@ -21,19 +21,19 @@ def update_score():
 	score.set(score.get() + 1)
 def bad_autocliker():
 	global score 
-	if score.get() > 1000:
+	if score.get() > 999:
 		while True:
 			root.update()
 			root.after(100)
 			root.update()
 			score.set(score.get() + 1)
 #Labels
-welcome_screen = Label(root, text = "welcome to cheese clicker!!").pack()
-score_display = Label(root, text = "your score is below this line:").pack()
-score_label = Label(root, textvariable = score).pack()
+welcome_screen = Label(root, text = "welcome to cheese clicker!!").grid(row = 0 , column = 0)
+score_display = Label(root, text = "your score is below this line:").grid(row = 1,column = 0)
+score_label = Label(root, textvariable = score).grid(row = 2, column = 0)
 #Buttons
-Cheese = Button(root, text = "tasty cheese", bg = "#ffa600", fg = "black", activebackground = "#ffa600", padx = 55 , pady= 55,command = update_score).pack()
-Buy_upgrade_1 = Button(root, text = "american cheese", bg = "#f3af41", fg = "black", activebackground = "#f3af41", padx = 50 , pady = 50,command = upgrade1).pack()
-blue_cheese = Button(root, text = "blue cheese" , bg = "blue", fg = "black", activebackground = "blue", padx = 50, pady = 50, command = upgrade).pack()
-autoclicker = Button(root, text = "autoclicker" , bg = "green" , fg = "black", activebackground = "green" , command = bad_autocliker).pack()
+Cheese = Button(root, text = "tasty cheese", bg = "#ffa600", fg = "black", activebackground = "#ffa600", padx = 55 , pady= 55,command = update_score).grid(row = 3, column = 3)
+Buy_upgrade_1 = Button(root, text = "american cheese", bg = "#f3af41", fg = "black", activebackground = "#f3af41", padx = 55 , pady = 55,command = upgrade1).grid(row = 3, column = 4)
+blue_cheese = Button(root, text = "blue cheese" , bg = "blue", fg = "black", activebackground = "blue", padx = 55, pady = 55, command = upgrade).grid(row = 4, column = 3)
+autoclicker = Button(root, text = "autoclicker" , bg = "green" , fg = "black", activebackground = "green" , command = bad_autocliker, padx = 55 ,pady = 55).grid(row = 4, column = 4)
 root.mainloop()
